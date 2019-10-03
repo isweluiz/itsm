6081043ec8a9365d3f90651ab756c4739b01b11b
Luiz Eduardo <isweluiz@hotmail.com>
2019-10-03 15:40:04 -0300

    Update Key Performance-Indicators-List.sql

--- KPI/Key Performance-Indicators-List.sql
+++ KPI/Key Performance-Indicators-List.sql
@@ -1,4 +1,6 @@
 ###GERAL
+##Percentual dos chamados que possuem SLA
+# NOME : % chamados encerrados que possuem SLA
 
 select 
 round((sum(case when sla_attend = 'true' then 1 else 0 end) * 100)/count(*),2) as perc_geral
@@ -6,33 +8,35 @@ from vw_kpi_tickets
 where 1=1
 and queue_finish is not null
 
-### CSTI
-
+### N1
+#Percentual dos chamados que possuem SLA
+## % SLA N1
 select 
 round((sum(case when sla_attend = 'true' then 1 else 0 end) * 100)/count(*),2) as perc_csti
 from vw_kpi_tickets
 where 1=1
-and queue_finish = 'Central de Serviços'
-
+and queue_finish = 'Suporte 1º Nível'
 
 ###N2
+#Percentual dos chamados que possuem SLA
+## % SLA N2
 select 
 round((sum(case when sla_attend = 'true' then 1 else 0 end) * 100)/count(*),2) as perc_csti
 from vw_kpi_tickets
 where 1=1
-and queue_finish = 'Tecnologia::Suporte Presencial'
-
-
+and queue_finish = 'Suporte 2º Nível'
 
 ###N3
+#Percentual dos chamados que possuem SLA
+## % SLA N3
 select 
 round((sum(case when sla_attend = 'true' then 1 else 0 end) * 100)/count(*),2) as perc_csti
 from vw_kpi_tickets
 where 1=1
-and queue_finish = 'Tecnologia::Rede'
+and queue_finish = 'Suporte 3º Nível'
 
 
-## SLA estourado 
+## % SLA estourado 
 
 select 
 round((sum(case when sla_attend = 'false' then 1 else 0 end) * 100)/count(*),2) as perc_geral
