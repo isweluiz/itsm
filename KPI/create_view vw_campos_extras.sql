e09cee0f67b4214477fb2858290ab5b4e1cdadf4
Luiz Eduardo <isweluiz@hotmail.com>
2020-08-21 23:44:02 -0300

    Update create_view vw_campos_extras.sql

--- KPI/create_view vw_campos_extras.sql
+++ KPI/create_view vw_campos_extras.sql
@@ -9,3 +9,10 @@ dfv1.object_id as tickect_id,
 (select dfv.value_text from dynamic_field_value dfv where dfv.field_id =16 and object_id =  dfv1.object_id ) as ramal2
 from dynamic_field_value dfv1
 group by object_id
+
+
+
+select 
+(select dfv1.value_text from dynamic_field_value dfv1 where df.id = dfv1.field_id and dfv1.field_id =13) as UST,
+(select dfv2.value_text from dynamic_field_value dfv2 where df.id = dfv2.field_id and dfv2.field_id =14) as Atividade
+from dynamic_field df
