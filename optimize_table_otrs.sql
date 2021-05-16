dbfb5c3d8587d2054ff5ddf1a8106f8585cf8b55
Luiz Eduardo <isweluiz@hotmail.com>
2021-05-15 23:59:08 -0300

    Create optimize_table_otrs.sql

---
+++ optimize_table_otrs.sql
@@ -0,0 +1,9 @@
+optimize table ticket;
+optimize table ticket_history ;
+optimize table service ;
+optimize table sla ;
+optimize table survey;
+
+optimize table survey_answer ;
+optimize table survey_question ;
+optimize table survey_vote ;
