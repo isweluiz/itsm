768cb41964b28952c936be9e25a4c24af0550b9a
Luiz Eduardo <isweluiz@hotmail.com>
2021-08-17 14:34:38 -0300

    Update report_communication_channel.sql

--- KPI/report_communication_channel.sql
+++ KPI/report_communication_channel.sql
@@ -4,3 +4,12 @@ select tn, create_time, title, communication_channel, type_name,
                 WHERE ((YEAR(create_time) = 2020 and MONTH(create_time) = 07)
                 or
                 (YEAR(create_time) = 2020 and MONTH(create_time) = 08));
+
+
+
+select tn, create_time,finish_time, title, communication_channel, type_name,
+                state_name,user_create,user_finish,queue_create,customer_user_id
+                FROM vw_kpi_tickets
+                WHERE ((YEAR(create_time) = 2021 and MONTH(create_time) = 08)
+                or
+                (YEAR(create_time) = 2021 and MONTH(create_time) = 08));
